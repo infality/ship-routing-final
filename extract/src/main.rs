933d0576141ad44a547d2f1308a74017b7ccf245
tomatze <tomatze@dismail.de>
2021-11-21 18:31:26 +0100

    removed unused/old code

--- extract/src/main.rs
+++ extract/src/main.rs
@@ -6,7 +6,6 @@ use std::{
     io::BufWriter,
     io::Error,
 };
-// TODO import of rayon does not work
 use rayon::prelude::*;
 
 const FACTOR: f64 = 10_000_000.0;
@@ -212,7 +211,6 @@ impl Coasts {
             features: Vec::new(),
         };
 
-        //let actual_coast = &self.actual_coasts[1];
         for actual_coast in self.actual_coasts.iter() {
             let mut coordinates = Vec::<[f64; 2]>::new();
 
@@ -305,77 +303,12 @@ impl Node {
                         intersection_count += 1;
                     }
                 }
-
-                //if (first.lon <= self.coordinate.lon && self.coordinate.lon < second.lon)
-                //    || (second.lon < self.coordinate.lon && self.coordinate.lon <= first.lon)
-                //    || (self.coordinate.get_lon() == 180.0 && second.get_lon() == 180.0)
-                //    || (self.coordinate.get_lon() == -180.0 && second.get_lon() == -180.0)
-                //{
-                //} else {
-                //    continue;
-                //}
-
-                //// Handle special case if line is vertical
-                //if first.lon == second.lon {
-                //    continue;
-                //}
-
-                ////if smaller_lon < 0 && larger_lon < 0 || smaller_lon >= 0 && larger_lon >= 0 {
-                ////    //both lons are negative or positive
-                ////    //shortest line between lons is not crossing antimeridian or 0-meridian (no problem)
-                ////    if !(smaller_lon <= self.coordinate.lon && self.coordinate.lon <= larger_lon) {
-                ////        continue;
-                ////    }
-                ////}
-                ////else {
-                ////    //one lon is negative and one lon is positive
-                ////    //println!("one is neg one is pos {}, {}", smaller_lon, larger_lon);
-                ////    if smaller_lon.abs() + larger_lon.abs() > 180*FACTOR as i32 {
-                ////        println!("crossing antimeridian {}, {}", smaller_lon, larger_lon);
-                ////        // shortest line between lons is crossing antimeridian (special case)
-                ////        if !(smaller_lon <= self.coordinate.lon && self.coordinate.lon <= -180*FACTOR as i32
-                ////             || 180*FACTOR as i32 <= self.coordinate.lon && self.coordinate.lon <= larger_lon) {
-                ////            continue;
-                ////        }
-                ////    }
-                ////    else {
-                ////        // shortest line between lons is crossing 0-meridian (no problem)
-                ////        if !(smaller_lon <= self.coordinate.lon && self.coordinate.lon <= larger_lon) {
-                ////            continue;
-                ////        }
-                ////    }
-                ////}
-
-                //let intersections =
-                //    calculate_intersections(&self.coordinate, &WATER, &first, &second);
-
-                ////nodes_intersections.nodes.push(Node {
-                ////    coordinate : intersections,
-                ////    is_water : true,
-                ////});
-
-                ////println!("{}, {}", intersections.get_lon(), intersections.get_lat());
-                //// Check if the intersection is on the coast line
-                //if (first.lon <= intersections.lon + 1 && intersections.lon - 1 < second.lon)
-                //    || (second.lon < intersections.lon + 1 && intersections.lon - 1 <= first.lon)
-                //{
-                //    if intersections.lat > self.coordinate.lat {
-                //        intersection_count += 1;
-                //        //println!("yes")
-                //    }
-                //} else {
-                //    //println!("nope")
-                //}
             }
-            //println!("{}", intersection_count);
             if intersection_count % 2 == 1 {
-                //println!("is uneven");
-                //println!("node is inside coastline-polygon {}", i);
                 self.is_water = false;
                 return;
             }
         }
-        //nodes_intersections.write_to_geojson("intersections.json");
     }
 }
 
@@ -416,25 +349,10 @@ impl Nodes {
         Nodes { nodes }
     }
 
-    fn new_generate_custom() -> Nodes {
-        println!("Generating not equally distributed nodes");
-        let mut nodes = Vec::new();
-
-        nodes.push(Node {
-            coordinate: Coordinate {
-                lon: 200000000,
-                lat: 300000000,
-            },
-            is_water: true,
-        });
-        Nodes { nodes }
-    }
-
     fn new_generate_not_equally_distributed() -> Nodes {
         println!("Generating not equally distributed nodes");
         let mut nodes = Vec::new();
 
-        // TODO Generate equally distributed nodes
         for lon in (0..=180).step_by(10) {
             for lat in (0..=90).step_by(10) {
                 nodes.push(Node {
@@ -508,79 +426,34 @@ impl Nodes {
     }
 }
 
-fn to_radians(value: f64) -> f64 {
-    value * std::f64::consts::PI / 180.0
-}
-
-fn to_degrees(value: f64) -> f64 {
-    value * 180.0 / std::f64::consts::PI
-}
-
-fn cross(a: [f64; 3], b: [f64; 3]) -> [f64; 3] {
-    [
-        a[1] * b[2] - a[2] * b[1],
-        a[2] * b[0] - a[0] * b[2],
-        a[0] * b[1] - a[1] * b[0],
-    ]
-}
-
-fn dot(a: [f64; 3], b: [f64; 3]) -> f64 {
-    a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
-}
-
-fn calculate_intersections(
-    p1: &Coordinate,
-    p2: &Coordinate,
-    p3: &Coordinate,
-    p4: &Coordinate,
-) -> Coordinate {
-    let p1_lon_rad = to_radians(p1.get_lon());
-    let p1_lat_rad = to_radians(p1.get_lat());
-    let p2_lon_rad = to_radians(p2.get_lon());
-    let p2_lat_rad = to_radians(p2.get_lat());
-    let p3_lon_rad = to_radians(p3.get_lon());
-    let p3_lat_rad = to_radians(p3.get_lat());
-    let p4_lon_rad = to_radians(p4.get_lon());
-    let p4_lat_rad = to_radians(p4.get_lat());
-
-    // Convert to vector
-    let v1_x = p1_lat_rad.cos() * p1_lon_rad.cos();
-    let v1_y = p1_lat_rad.cos() * p1_lon_rad.sin();
-    let v1_z = p1_lat_rad.sin();
-    let v2_x = p2_lat_rad.cos() * p2_lon_rad.cos();
-    let v2_y = p2_lat_rad.cos() * p2_lon_rad.sin();
-    let v2_z = p2_lat_rad.sin();
-    let v3_x = p3_lat_rad.cos() * p3_lon_rad.cos();
-    let v3_y = p3_lat_rad.cos() * p3_lon_rad.sin();
-    let v3_z = p3_lat_rad.sin();
-    let v4_x = p4_lat_rad.cos() * p4_lon_rad.cos();
-    let v4_y = p4_lat_rad.cos() * p4_lon_rad.sin();
-    let v4_z = p4_lat_rad.sin();
-
-    // Get great-circles
-    let gc1 = cross([v1_x, v1_y, v1_z], [v2_x, v2_y, v2_z]);
-    let gc2 = cross([v3_x, v3_y, v3_z], [v4_x, v4_y, v4_z]);
-
-    // Get intersection points
-    let i1 = cross(gc1, gc2);
-    let i2 = cross(gc2, gc1);
-
-    // Find nearest intersection and convert back to lat/lon
-    let mid = dot(cross(gc1, [v1_x, v1_y, v1_z]), i1);
-    if mid > 0.0 {
-        let lat1 = to_degrees(f64::atan2(i1[2], f64::sqrt(i1[0].powi(2) + i1[1].powi(2))));
-        let lon1 = to_degrees(f64::atan2(i1[1], i1[0]));
-        Coordinate {
-            lon: (lon1 * FACTOR) as i32,
-            lat: (lat1 * FACTOR) as i32,
-        }
+fn transform_lon(p: &Coordinate, q: &Coordinate) -> f64 {
+    if p.lat == 900000000 {
+        return q.get_lon();
     } else {
-        let lat2 = to_degrees(f64::atan2(i2[2], f64::sqrt(i2[0].powi(2) + i2[1].powi(2))));
-        let lon2 = to_degrees(f64::atan2(i2[1], i2[0]));
-        Coordinate {
-            lon: (lon2 * FACTOR) as i32,
-            lat: (lat2 * FACTOR) as i32,
-        }
+        let plon_rad = p.get_lon().to_radians();
+        let plat_rad = p.get_lat().to_radians();
+        let qlon_rad = q.get_lon().to_radians();
+        let qlat_rad = q.get_lat().to_radians();
+        let t = (qlon_rad - plon_rad).sin() * qlat_rad.cos();
+        let b = qlat_rad.sin() * plat_rad.cos()
+            - qlat_rad.cos() * plat_rad.sin() * (qlon_rad - plon_rad).cos();
+        return f64::atan2(t, b).to_degrees();
+    }
+}
+
+fn east_or_west(clon: f64, dlon: f64) -> i32 {
+    let mut del = dlon - clon;
+    if del > 180.0 {
+        del = del - 360.0;
+    } else if del < -180.0 {
+        del = del + 360.0;
+    }
+    if del > 0.0 && del != 180.0 {
+        return -1;
+    } else if del < 0.0 && del != -180.0 {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
@@ -617,7 +490,7 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    let mut coasts;
+    let coasts;
     if !skip_read_pbf {
         coasts = Coasts::new_from_pbffile(&file_name);
         coasts.write_to_geojson("coastlines.json");
@@ -626,33 +499,6 @@ fn main() -> Result<(), Error> {
         coasts = Coasts::new_from_binfile(&file_name);
         //coasts.write_to_geojson("coastlines.json");
     }
-    let custom_coast = Coast {
-        coordinates: vec![
-            Coordinate {
-                lon: -100000000,
-                lat: 340000000,
-            },
-            Coordinate {
-                lon: 570000000,
-                lat: 490000000,
-            },
-            Coordinate {
-                lon: 126000000,
-                lat: 200000000,
-            },
-            Coordinate {
-                lon: 380000000,
-                lat: -110000000,
-            },
-        ],
-        leftmost: -100000000,
-        rightmost: 570000000,
-    };
-    //coasts.actual_coasts.push(custom_coast);
-    //coasts = Coasts {
-    //    actual_coasts : vec![custom_coast],
-    //};
-    //coasts.write_to_geojson("coastlines-custom.json");
 
     for coast in coasts.actual_coasts.iter() {
         if coast.coordinates.len() > 500000 {
@@ -669,94 +515,17 @@ fn main() -> Result<(), Error> {
     //let mut nodes = Nodes::new_generate_custom();
     //nodes.write_to_geojson("grid.json");
 
-    //let mut nodes = Nodes::new_generate_equally_distributed();
-    /* let mut nodes = Nodes {
-        nodes: vec![Node {
-            coordinate: Coordinate {
-                lon: 180_0000000,
-                lat: -80_0000000,
-            },
-            is_water: true,
-        }],
-    }; */
-
-    let mut counter = 0;
-    let node_count = nodes.nodes.len();
+    //let mut counter = 0;
+    //let node_count = nodes.nodes.len();
     nodes.nodes.par_iter_mut().for_each(|node| {
         /* if counter % 10 == 0 {
             println!("Setting water flags: {}/{}", counter, node_count);
         }
         counter += 1; */
         node.set_water_flag(&coasts);
-        //node.set_water_flag_spherical(&coasts);
     });
 
     nodes.write_to_geojson("nodes.json");
 
     Ok(())
-}
-
-fn transform_lon(p: &Coordinate, q: &Coordinate) -> f64 {
-    if p.lat == 900000000 {
-        return q.get_lon();
-    } else {
-        let plon_rad = p.get_lon().to_radians();
-        let plat_rad = p.get_lat().to_radians();
-        let qlon_rad = q.get_lon().to_radians();
-        let qlat_rad = q.get_lat().to_radians();
-        let t = (qlon_rad - plon_rad).sin() * qlat_rad.cos();
-        let b = qlat_rad.sin() * plat_rad.cos()
-            - qlat_rad.cos() * plat_rad.sin() * (qlon_rad - plon_rad).cos();
-        return f64::atan2(t, b).to_degrees();
-    }
-}
-
-fn east_or_west(clon: f64, dlon: f64) -> i32 {
-    let mut del = dlon - clon;
-    if del > 180.0 {
-        del = del - 360.0;
-    } else if del < -180.0 {
-        del = del + 360.0;
-    }
-    if del > 0.0 && del != 180.0 {
-        return -1;
-    } else if del < 0.0 && del != -180.0 {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-#[test]
-fn test_intersections() {
-    /* assert_eq!(
-        calculate_intersections(
-            &Coordinate {
-                lat: 330000000,
-                lon: -240000000,
-            },
-            &Coordinate {
-                lat: 520000000,
-                lon: -700000000,
-            },
-            &Coordinate {
-                lat: 490000000,
-                lon: 10000000,
-            },
-            &Coordinate {
-                lat: 460000000,
-                lon: 150000000,
-            },
-        ),
-        (
-            Coordinate {
-                lat: -467105560,
-                lon: 1668375000,
-            },
-            Coordinate {
-                lat: 467105560,
-                lon: -131625000,
-            }
-        )
-    ); */
 }
